8297848238b5e331390ceaab95e862f459ece484
Sebastião Relson <relsonto@gmail.com>
2023-12-01 16:30:34 -0300

    reset tabela

--- src/db/busca.sql
+++ src/db/busca.sql
@@ -92,7 +92,7 @@ CREATE TABLE `palavra` (
   `palavra` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `palavra_unq_idx` (`palavra`)
-) ENGINE=InnoDB AUTO_INCREMENT=3386 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
